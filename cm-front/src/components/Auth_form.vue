1b177e2760a3c320c5486d82e87f803011a8d70e
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-07-05 17:25:10 +0300

    extract auth_form and footer in component

---
+++ cm-front/src/components/Auth_form.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="auth">
+        <form class="form-signin">
+            <h2 class="h2 mb-3 font-weight-normal">Please sign in</h2>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me">Remember me </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <router-link class="cm-form-signin__link" to="/forgotpassword">Forgot password?</router-link>
+            <router-link class="cm-form-signin__link" to="/registration">Don`t have an account?</router-link>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+  name: "Auth_form"
+};
+</script>
+<style scoped lang="scss">
+body {
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+  text-align: center;
+  margin-top: 50px;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
---
+++ cm-front/src/components/Footer_static.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="footer__nav">
+        <ul class="footer__list">
+            <li class="footer__item"><a href="#" class="footer__link">Footer item 1</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Footer item 2</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Footer item 3</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Footer item 4</a></li>
+            <li class="footer__item footer__item--logo">
+                <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+  name: "footer_static"
+};
+</script>
+<style lang="scss">
+@import "../../node_modules/bootstrap/scss/bootstrap-reboot";
+@import "../../node_modules/bootstrap/scss/bootstrap";
+@import "../../node_modules/bootstrap/scss/bootstrap-grid";
+
+body {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+footer {
+  background-color: #343a40;
+  margin-top: auto;
+}
+
+.nav-link {
+  background-image: url(../assets/logo.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+</style>
