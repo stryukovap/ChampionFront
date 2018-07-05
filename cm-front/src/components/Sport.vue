1377ea6e7d27403e31e6dccfbb071e3a2864fd24
Марина Буякевич <manyunya1011@gmail.com>
2018-07-05 18:24:41 +0000

    Upload New File

---
+++ cm-front/src/components/Sport.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-md-2">
+        <div class="thumbnail">    
+            <img src="img/github-mark_560x560.png">
+            <div class="caption">
+                <h4 class="text-center">Denis Yermolin</h4>
+                <p class="text-center">Coach<img src="img/pos.png">2 Dan</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
