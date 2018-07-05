28ebb4c424249395357e4ba0cb7cf4580284b124
Марина Буякевич <manyunya1011@gmail.com>
2018-07-05 20:28:26 +0000

    Update Sport.vue

--- cm-front/src/components/Sport.vue
+++ cm-front/src/components/Sport.vue
@@ -5,7 +5,7 @@
                 <img src="../assets/github-mark_560x560.png">
                 <div class="caption">
                     <h4 class="text-center">Denis Yermolin</h4>
-                    <p class="text-center">Coach<img src="img/pos.png">2 Dan</p>
+                    <p class="text-center">Coach<img src="../assets/pos.png">2 Dan</p>
                 </div>
             </div>
         </div>
