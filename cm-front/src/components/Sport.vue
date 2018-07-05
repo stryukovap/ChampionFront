9427b9a652eb0db2e968be7c4412d3149d983d3a
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-07-05 21:58:31 +0300

    fix error in added components

--- cm-front/src/components/Sport.vue
+++ cm-front/src/components/Sport.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="col-md-2">
-        <div class="thumbnail">    
-            <img src="img/github-mark_560x560.png">
-            <div class="caption">
-                <h4 class="text-center">Denis Yermolin</h4>
-                <p class="text-center">Coach<img src="img/pos.png">2 Dan</p>
+    <div class="sport">
+        <div class="col-md-2">
+            <div class="thumbnail">
+                <img src="img/github-mark_560x560.png">
+                <div class="caption">
+                    <h4 class="text-center">Denis Yermolin</h4>
+                    <p class="text-center">Coach<img src="img/pos.png">2 Dan</p>
+                </div>
             </div>
         </div>
     </div>
@@ -12,11 +14,9 @@
 
 <script>
 export default {
-
-}
+  name: "sport"
+};
 </script>
 
 <style>
-
 </style>
-
