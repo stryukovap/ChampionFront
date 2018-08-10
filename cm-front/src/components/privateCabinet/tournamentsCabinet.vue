f38cc22e583df83c681bf499000f1487f09391d9
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-08-10 16:15:04 +0300

    same bugs merge

--- cm-front/src/components/privateCabinet/tournamentsCabinet.vue
+++ cm-front/src/components/privateCabinet/tournamentsCabinet.vue
@@ -21,93 +21,96 @@
                              alt="foto referee"
                              class="card__img">
                         <span class="card__desc">{{referee.desc}}</span>
-                        <div>
-                            <div v-if="tournamentsShow">
-                                <div class="row mt-4 mr-4">
-                                    <div class="col-12 text-right">
-                                        <button class="btn btn-outline-success" @click="createTournament" type="submit">
-                                            Create
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="col-xs-12 col-md-9">
-                                    <div class="row">
-                                        <div class="col-xs-12 col-sm-4"
-                                             v-for='(tournament, key) in $store.state.tournamentsList'>
-                                            <div class="thumbnail">
-                                                <img class="card-img-top"
-                                                     src="../../assets/github-mark_560x560.png"
-                                                     alt="Card image cap">
-                                                <div class="caption mb-3">
-                                                    <h4 @click="openTournament(key)" class="text-center">{{
-                                                        tournament.name }}</h4>
-                                                    <p class="text-center mb-1">Start date: {{
-                                                        tournament.dates.dateStart }}</p>
-                                                </div>
-                                            </div>
-                                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+                <div v-if="tournamentsShow">
+                    <div class="row mt-4 mr-4">
+                        <div class="col-12 text-right">
+                            <button class="btn btn-outline-success" @click="createTournament" type="submit">
+                                Create
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-9">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-4"
+                                 v-for='(tournament, key) in $store.state.tournamentsList'>
+                                <div class="thumbnail">
+                                    <img class="card-img-top"
+                                         src="../../assets/github-mark_560x560.png"
+                                         alt="Card image cap">
+                                    <div class="caption mb-3">
+                                        <h4 @click="openTournament(key)" class="text-center">{{
+                                            tournament.name }}</h4>
+                                        <p class="text-center mb-1">Start date: {{
+                                            tournament.dates.dateStart }}</p>
                                     </div>
                                 </div>
                             </div>
-                            <Tabs>
-                                <Tab name="Brackets">Brackets</Tab>
-                                <Tab name="Participants">
-                                    <div class="row mt-2">
-                                        <div class="col-9">
-                                            <nav class="navbar navbar-light">
-                                                <form class="form-inline">
-                                                    <input class="form-control mr-sm-2" type="search"
-                                                           placeholder="Enter name" aria-label="Search">
-                                                    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
-                                                        Search
-                                                    </button>
-                                                </form>
-                                            </nav>
-                                        </div>
-                                        <div class="col-2">
-                                            <button type="button" class="btn btn-outline-primary">Save Changes</button>
-                                        </div>
-                                        <div class="col-1">
-                                            <button type="button" class="btn btn-outline-primary">Add</button>
-                                        </div>
-                                    </div>
-
-                                    <div class="row ml-2">
-                                        <table class="table table-hover table-sm">
-                                            <tbody :list="$store.state.sportsmanList">
-                                            <tr class="row" v-for="item in $store.state.sportsmanList" :key="item.id">
-                                                <td class="col-2 text-left">{{item.first_name}} {{item.last_name}}</td>
-                                                <td class="col-1 text-left">{{item.gender}}</td>
-                                                <td class="col-2 text-left">{{item.date_of_birth}}</td>
-                                                <td class="col-1 text-left"><input class="form-control form-control-sm"
-                                                                                   width="50px"></td>
-                                                <td class="col-5 text-right">
-                                                    <button type="button" class="btn btn-outline-primary btn-sm">
-                                                        Submit
-                                                    </button>
-                                                </td>
-                                                <td class="col-1 text-left">
-                                                    <button type="button" class="btn btn-outline-primary btn-sm">
-                                                        Delete
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </Tab>
-                            </Tabs>
-                            <tournament-page
-                                    v-if="tournamentPageShow"
-                                    v-bind:tournament-key="tournamentKey"
-                                    @click.prevent="closeTournament">
-                            </tournament-page>
-                            <tournament-create
-                                    v-if="modalShow"
-                                    @click.prevent="closeModal"
-                                    @clicked="closeAndUpdate">
-                            </tournament-create>
                         </div>
+                    </div>
+                </div>
+                <Tabs>
+                    <Tab name="Brackets">Brackets</Tab>
+                    <Tab name="Participants">
+                        <div class="row mt-2">
+                            <div class="col-9">
+                                <nav class="navbar navbar-light">
+                                    <form class="form-inline">
+                                        <input class="form-control mr-sm-2" type="search"
+                                               placeholder="Enter name" aria-label="Search">
+                                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
+                                            Search
+                                        </button>
+                                    </form>
+                                </nav>
+                            </div>
+                            <div class="col-2">
+                                <button type="button" class="btn btn-outline-primary">Save Changes</button>
+                            </div>
+                            <div class="col-1">
+                                <button type="button" class="btn btn-outline-primary">Add</button>
+                            </div>
+                        </div>
+
+                        <div class="row ml-2">
+                            <table class="table table-hover table-sm">
+                                <tbody :list="$store.state.sportsmanList">
+                                <tr class="row" v-for="item in $store.state.sportsmanList" :key="item.id">
+                                    <td class="col-2 text-left">{{item.first_name}} {{item.last_name}}</td>
+                                    <td class="col-1 text-left">{{item.gender}}</td>
+                                    <td class="col-2 text-left">{{item.date_of_birth}}</td>
+                                    <td class="col-1 text-left"><input class="form-control form-control-sm"
+                                                                       width="50px"></td>
+                                    <td class="col-5 text-right">
+                                        <button type="button" class="btn btn-outline-primary btn-sm">
+                                            Submit
+                                        </button>
+                                    </td>
+                                    <td class="col-1 text-left">
+                                        <button type="button" class="btn btn-outline-primary btn-sm">
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </Tab>
+                </Tabs>
+                <tournament-page
+                        v-if="tournamentPageShow"
+                        v-bind:tournament-key="tournamentKey"
+                        @click.prevent="closeTournament">
+                </tournament-page>
+                <tournament-create
+                        v-if="modalShow"
+                        @click.prevent="closeModal"
+                        @clicked="closeAndUpdate">
+                </tournament-create>
+            </div>
 </template>
 <script>
     import axios from "axios";
@@ -178,36 +181,8 @@
             },
         }
     }
-
-    <!--export default {-->
-    <!--components: {-->
-
-    <!--},-->
-    <!--data: function () {-->
-    <!--return {-->
-
-    <!--tournament: {-->
-    <!--referees: [-->
-    <!--{-->
-    <!--id: 1,-->
-    <!--desc: "Denis Ermolin",-->
-    <!--},-->
-    <!--{-->
-    <!--id: 2,-->
-    <!--desc: "Ermolin Denis",-->
-    <!--},-->
-    <!--{-->
-    <!--id: 3,-->
-    <!--desc: "Nastya Kolobok",-->
-    <!--}],-->
-    <!--title: "White Lion Cup",-->
-    <!--description: "lorem lorem lorem lorem lorem lorem lorem",-->
-    <!--dateStart: "01.07.2018",-->
-    <!--dateEnd: "21.07.2018",-->
-    <!--},-->
-    <!--}},}-->
 </script>
-<!---->
+
 <style lang="scss">
     .table {
         &__head-item {
