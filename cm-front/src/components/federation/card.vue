a14ae9f86d6266cc92d251bb4fa51f006b0e2382
VitaliyFEP <vitalik_p@hotmail.com>
2018-08-23 15:54:34 +0300

    integrated tournaments

--- cm-front/src/components/federation/card.vue
+++ cm-front/src/components/federation/card.vue
@@ -1,19 +1,19 @@
 <template>
     <section>
         <div class="container">
+            <h2>Nearest tournaments</h2>
             <div class="row">
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-4"
+                    v-for='(tournament, key) in $store.state.tournamentsList'>
                     <div class="thumbnail">
                         <img class="card-img-top"
                              src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16345a6903e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16345a6903e%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.203125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
                              alt="Card image cap">
                         <div class="caption">
-                            <h4 class="text-center">Tournament Name</h4>
-                            <p class="text-center">Start Date<br>
-                                Registration starts/finishes<br>
-                            </p>
-                            <p class="text-center">5/100 Members
-                            </p>
+                            <h4 class="text-center" >{{ tournament.name }}</h4>
+                            <p class="text-center">Start date: {{ tournament.dates.dateStart }}</p>
+                            <p class="text-center">Registration starts/finishes : {{tournament.dates.dateStart}}/{{tournament.dates.dateEnd}}</p>
+                            <p class="text-center">{{ tournament.maxParticipants }} Members</p>
                         </div>
                     </div>
                 </div>
@@ -23,9 +23,82 @@
 </template>
 
 <script>
+import axios from "axios";
+import * as firebase from "firebase";
+// import TournamentPage from "./tournamentPage";
+// import TournamentCreate from "./tournamentCreate";
 export default {
-  name: "card"
-};
+    name: "card",
+
+    data: function () {
+        // console.log(tournamentKey);
+        return {
+            tournamentsShow: true,
+            tournamentPageShow: false,
+            // tournamentKey: "",
+            federationId: this.$route.params.id,
+        };
+    },
+
+    // beforeMount: function () {
+    //     this.federationId = this.$store.state.authUser.federation_users[0].federation_id;
+    // },
+
+    async mounted() {
+        try {
+            const fbObj = await firebase
+                .database()
+                .ref(this.federationId)
+                .once("value");
+            this.$store.commit("setTournamentsList", fbObj.val());
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+  // {
+ //       openTournament(key) {
+  //          this.tournamentKey = key;
+  //          this.tournamentsShow = false;
+  //          this.tournamentPageShow = true;
+  //      },
+  //      closeTournament() {
+  //          this.tournamentPageShow = false;
+  //          this.tournamentsShow = true;
+  //      },
+  //      createTournament() {
+   //         this.modalShow = true;
+   //     },
+   //     closeModal() {
+   //         this.modalShow = false;
+   //     },
+   //     async closeAndUpdate() {
+         //   this.modalShow = false;
+   //         try {
+   //             const fbObj = await firebase
+   //                .database()
+   //                 .ref(this.federationId)
+   //                  .once("value");
+   //              this.$store.commit("setTournamentsList", fbObj.val());
+   //          } catch (error) {
+   //              throw error;
+   //         }
+   //      }
+   //  };
+   //  .then(response => {
+   //      for (let i = 0; i < response.data.data.length; i++) {
+   //          window.console.log(response.data.data[i]);
+   //          if(page !== "") this.sportsmenList.shift();
+   //          this.sportsmenList.push({
+   //              name: response.tournament.name,
+   //              dateStart: "tournament.dates.dateStart",
+   //              dateEnd: "tournament.dates.dateEnd",
+   //              Members: tournament.maxParticipants,
+   //          });
+   //      }
+   //  })
+
+
 </script>
 
 <style scoped>
--- cm-front/src/views/Federation.vue
+++ cm-front/src/views/Federation.vue
@@ -2,11 +2,10 @@
     <div>
       <div class="container">
         <div class="row">
-          <tournaments v-bind:tournamentsList="tournamentsList" class="col-lg-9"></tournaments>
+          <card class="col-lg-9"></card>
           <news v-bind:newsList="newsList" class="col-lg-3"></news>
         </div>
       </div>
-      <!-- <sportsmen v-bind:sportsmenList="sportsmenList"></sportsmen> -->
       <sportsmen></sportsmen>
     </div>
 </template>
@@ -16,95 +15,21 @@ import tournaments from "../components/federation/Tournaments";
 import sportsmen from "../components/federation/Sportsmen";
 import news from "../components/federation/News";
 import * as firebase from "firebase";
+import card from "../components/federation/card";
 
 export default {
   components: {
     tournaments,
     sportsmen,
-    news
+    news,
+    card
   },
   data: function() {
     return {
       newsList: [],
-      // sportsmenList: [],
-      tournamentsList: []
     };
   },
   async mounted() {
-    axios
-      .get(
-        // "https://champion-api.herokuapp.com/api/federation/" +
-        "http://champion-api.herokuapp.com/api/sportsman-list/by-federation/" + this.$route.params.id + "/6"
-          // this.$route.params.id
-      )
-      .then(response => {
-        // window.console.log(response.data.data)
-        // for (let i = 0; i < response.data.data.length; i++) {
-        //   window.console.log(response.data.data[i])
-        //   this.sportsmenList.push({
-        //     name: response.data.data[i].first_name + " " + response.data.data[i].last_name,
-        //     role: "coach",
-        //     belt: "../img/pos.png",
-        //     dan: 3,
-        //     link: "#",
-        //     avatar: "../img/user-photo.PNG"
-        //   });
-        // }
-      // .then(response => {
-      //   window.console.log(response.data.data)
-      //   for (let i = 0; i < response.data.sportsmen.length; i++) {
-      //     this.sportsmenList.push({
-      //       name: response.data.sportsmen[i].name,
-      //       role: response.data.sportsmen[i].role,
-      //       avatar: response.data.sportsmen[i].ava,
-      //       dan: response.data.sportsmen[i].rank,
-      //       link: response.data.sportsmen[i].link
-      //     });
-      //   }
-        // for (let i = 0; i < response.data.news.length; i++) {
-        //   this.newsList.push({
-        //     name: response.data.news[i].name
-        //   });
-        // }
-        // for (let i = 0; i < response.data.tournaments.length; i++) {
-        //   this.newsList.push({
-        //     name: response.data.tournaments[i].name
-        //   });
-        // }
-      })
-      .catch(error => {
-        window.console.log(error);
-        // for (let i = 0; i < 10; i++) {
-        //   this.sportsmenList.push({
-        //     avatar: "../img/user-photo.PNG",
-        //     name: "sp1",
-        //     role: "coach",
-        //     belt: "../img/pos.png",
-        //     dan: 3,
-        //     link: "#"
-        //   });
-        // }
-        // for (let i = 0; i < 5; i++) {
-        //   this.newsList.push({
-        //     name: "post name",
-        //     avatar:
-        //       "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16345a6903e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16345a6903e%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.203125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-        //     date: "13.13.18",
-        //     link: "#"
-        //   });
-        // }
-        for (let i = 0; i < 11; i++) {
-          this.tournamentsList.push({
-            avatar:
-              "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16345a6903e%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16345a6903e%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.203125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-            name: "tournament name",
-            date: "04.04.04",
-            registration: "02.02.02 - 03.03.03",
-            numberOfMembers: "5/100",
-            link: "#"
-          });
-        }
-      });
       try {
         const fbObj = await firebase
             .database()
