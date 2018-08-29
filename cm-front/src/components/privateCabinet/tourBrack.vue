61da65a3b6b34e0959edd19246f50871f5ec66cc
VitaliyFEP <vitalik_p@hotmail.com>
2018-08-29 22:38:35 +0300

    bracket small fix, add Sasha's styles

--- cm-front/src/components/privateCabinet/tourBrack.vue
+++ cm-front/src/components/privateCabinet/tourBrack.vue
@@ -9,8 +9,8 @@
                 <div class = 'game'
                      v-for = 'fight in round'
                      :key = 'fight.index'>
-                    <div class = 'player'>{{ (fight.fighter1 !== "empty" && fight.fighter1) ? fight.fighter1.fullname : '' }}</div>
-                    <div class = 'player'>{{ (fight.fighter2 !== "empty" && fight.fighter2) ? fight.fighter2.fullname : '' }}</div>
+                    <div class = 'player'>{{ fight.fighter1 ? fight.fighter1.fullname : '' }}</div>
+                    <div class = 'player'>{{ fight.fighter2 ? fight.fighter2.fullname : '' }}</div>
                 </div>
             </div>
         </div>
@@ -35,119 +35,124 @@
 </script>
 
 <style lang="scss">
-    .bracket {
-        display    : flex;
-        overflow-x : scroll;
+.bracket {
+    display    : flex;
+    overflow-x : auto;
+}
+
+.bracket-wrapper {
+    position : relative;
+    display    : flex;
+
     }
 
-    .bracket-wrapper {
-        position : relative;
-        display    : flex;
-
+.round {
+    display         : flex;
+    flex-direction  : column;
+    justify-content : space-around;
+    margin-top : 30px;
+    margin-right : 30px;
     }
 
-    .round {
-        display         : flex;
-        flex-direction  : column;
-        justify-content : space-around;
-        margin-top : 30px;
-        margin-right : 30px;
+.last-round {
+    justify-content: center;
     }
 
-    .round-name {
-        position : absolute;
-        top : 0;
-        text-align: center;
-        width : 150px;
-        background-color : #ccc;
-        border-radius: 5px;
+.round-name {
+    position : absolute;
+    top : 0;
+    text-align: center;
+    width : 200px;
+    margin-left : 10px;
+    background-color : #ccc;
+    border-radius: 5px;
     }
 
-    .round:nth-last-child(2) {
-        position : absolute;
-        right : 0;
+.third-place,
+.final {
+    position : static;
+    margin-top : 50px;
     }
 
-    /*@supports (margin-bottom: var(--prop)) {*/
-    /*.round:nth-last-child(2){*/
-    /*bottom : var(--margin_3rd_place);*/
-    /*}*/
-    /*}*/
-
-    .game {
-        width       : 200px;
-        border      : 2px solid #5CB85C;
-        border-radius: 10px ;
-        line-height : 2em;
-        text-align  : center;
-        padding     : 0 10px;
-        margin      : 10px;
-        position    : relative;
+.game {
+    width       : 200px;
+    border      : 2px solid #5CB85C;
+    border-radius: 10px ;
+    line-height : 2em;
+    text-align  : center;
+    padding     : 0 10px;
+    margin      : 10px;
+    position    : relative;
     }
 
-    .game::after {
-        display          : block;
-        content          : '';
-        position         : absolute;
-        right            : -52px;
-        top              : 50%;
-        background-color : #5CB85C;
-        width            : 51px;
-        height           : 3px;
-        transform-origin : 0 0;
+.game::after {
+    display          : block;
+    content          : '';
+    position         : absolute;
+    right            : -52px;
+    top              : 50%;
+    background-color : #5CB85C;
+    width            : 51px;
+    height           : 3px;
+    transform-origin : 0 0;
     }
 
-    .game:nth-of-type(odd)::after {
-        transform : skewY(38deg);
+.game:nth-of-type(odd)::after {
+    transform : skewY(38deg);
     }
 
-    .game:nth-of-type(even)::after {
-        transform : skewY(-38deg);
+.game:nth-of-type(even)::after {
+    transform : skewY(-38deg);
     }
 
-    .round:nth-child(2) .game:nth-of-type(odd)::after {
-        transform : skewY(59deg)
+.round:nth-child(2) .game:nth-of-type(odd)::after {
+    transform : skewY(59deg)
     }
 
-    .round:nth-child(2) .game:nth-of-type(even)::after {
-        transform : skewY(-59deg)
+.round:nth-child(2) .game:nth-of-type(even)::after {
+    transform : skewY(-59deg)
     }
 
-    .round:nth-child(3) .game:nth-of-type(odd)::after {
-        transform : skewY(73.5deg)
+.round:nth-child(3) .game:nth-of-type(odd)::after {
+    transform : skewY(73.5deg)
     }
 
-    .round:nth-child(3) .game:nth-of-type(even)::after {
-        transform : skewY(-73.5deg)
+.round:nth-child(3) .game:nth-of-type(even)::after {
+    transform : skewY(-73.5deg)
     }
 
-    .round:nth-child(4) .game:nth-of-type(odd)::after {
-        transform : skewY(81.8deg)
+.round:nth-child(4) .game:nth-of-type(odd)::after {
+    transform : skewY(81.8deg)
     }
 
-    .round:nth-child(4) .game:nth-of-type(even)::after {
-        transform : skewY(-81.8deg)
+.round:nth-child(4) .game:nth-of-type(even)::after {
+    transform : skewY(-81.8deg)
     }
 
-    .round:nth-child(5) .game:nth-of-type(odd)::after {
-        transform : skewY(85.8deg)
+.round:nth-child(5) .game:nth-of-type(odd)::after {
+    transform : skewY(85.8deg)
     }
 
-    .round:nth-child(5) .game:nth-of-type(even)::after {
-        transform : skewY(-85.8deg)
+.round:nth-child(5) .game:nth-of-type(even)::after {
+    transform : skewY(-85.8deg)
     }
 
-    .round:nth-last-child(1) .game::after,
-    .round:nth-last-child(2) .game::after{
-        display : none;
+.round:nth-last-child(1) .game::after,
+.round:nth-last-child(2) .game::after{
+display : none;
     }
 
-    .player {
-        height : 2em;
-        overflow: hidden;
+.player {
+    height : 2em;
+    overflow: hidden;
     }
 
-    .player:nth-of-type(odd) {
-        border-bottom: 1px solid #5CB85C;
+.player:nth-of-type(odd) {
+    border-bottom: 1px solid #5CB85C;
+    }
+
+.fighter1,
+.fighter2 {
+    background-color : #5CB85C;
     }
 </style>
