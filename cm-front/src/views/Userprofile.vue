0e986c2bb4292c371d2a8bf51c99ebaa82597b33
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-08-31 21:53:00 +0300

    change width sportsman card in federation

--- cm-front/src/views/Userprofile.vue
+++ cm-front/src/views/Userprofile.vue
@@ -2,7 +2,7 @@
   <div class="container cm-co-profile">
 
     <user-info class="user-info"></user-info>
-    <user-certificates></user-certificates>
+    <!--<user-certificates></user-certificates>-->
 
     <!--<user-team v-if="userIsCoach"></user-team>-->
     <!--<user-history></user-history>-->
@@ -12,7 +12,7 @@
 <script>
 import axios from "axios";
 import userInfo from "../components/userProfile/userInfo";
-import userCertificates from "../components/userProfile/userCertificates";
+// import userCertificates from "../components/userProfile/userCertificates";
 // import userHistory from "../components/userProfile/userHistory";
 // import userTeam from "../components/userProfile/userTeam";
 
@@ -20,7 +20,7 @@ export default {
   name: "user-profile",
   components: {
     userInfo,
-    userCertificates,
+    // userCertificates,
     // userHistory,
     // userTeam
   },
