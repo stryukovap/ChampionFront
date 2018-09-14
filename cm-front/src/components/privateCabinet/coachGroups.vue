5e4cfd2a057877703fffa77961335e7ddaba3bd9
VitaliyFEP <vitalik_p@hotmail.com>
2018-09-14 21:46:50 +0300

    add group to privateCabinet

---
+++ cm-front/src/components/privateCabinet/coachGroups.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- <group-list></group-list> -->
+        <attendance></attendance>
+    </div>
+</template>
+
+<script>
+    import Attendance from './Groups/Attendance';
+
+    export default {
+        name: "coach-groups",
+        components: {
+            Attendance
+        }
+    }    
+</script>
+
+<style lang="scss">
+
+</style>
